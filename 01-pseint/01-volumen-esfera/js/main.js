c7a03e42ab6107f1b5307d16b7a912b5cc5f60d8
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-11-19 10:47:05 -0600

    fix: show only two decimal places

--- 01-pseint/01-volumen-esfera/js/main.js
+++ 01-pseint/01-volumen-esfera/js/main.js
@@ -1,5 +1,5 @@
 function calcularVolumen() {
     let radio = document.querySelector('#radio').value;
-    let volumen = (4 / 3) * Math.PI * Math.pow(radio, 3);
+    let volumen = ((4 / 3) * Math.PI * Math.pow(radio, 3)).toFixed(2);
     document.querySelector('#resultado').innerHTML = "El volúmen de la esfera con radio " + radio + " cm, es " + volumen + " cm^3.";
 }
