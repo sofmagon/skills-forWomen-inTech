5d971b8c16de6519918c092afc67181370c0e1dd
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-11-19 10:42:51 -0600

    initial commit

---
+++ 01-pseint/01-volumen-esfera/js/main.js
@@ -0,0 +1,5 @@
+function calcularVolumen() {
+    let radio = document.querySelector('#radio').value;
+    let volumen = (4 / 3) * Math.PI * Math.pow(radio, 3);
+    document.querySelector('#resultado').innerHTML = "El volúmen de la esfera con radio " + radio + " cm, es " + volumen + " cm^3.";
+}
