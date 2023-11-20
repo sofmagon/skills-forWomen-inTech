2730d76f177326c487edfff54f0751cdf61cdd9e
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-11-20 10:12:34 -0600

    initial commit

---
+++ 01-pseint/06-leer-nombre/js/main.js
@@ -0,0 +1,4 @@
+function leerNombre() {
+    let nombre = document.querySelector('#nombre').value;
+    document.querySelector('#resultado').innerHTML = "¡Bienvenidx a Hackademy " + nombre + "! 😎";
+}
