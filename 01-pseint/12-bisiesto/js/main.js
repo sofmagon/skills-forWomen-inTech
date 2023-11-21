0f8e0dcacdb882ecdf72ccc5fc93bcd86500616f
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-11-20 18:34:04 -0600

    initial commit

---
+++ 01-pseint/12-bisiesto/js/main.js
@@ -0,0 +1,9 @@
+let anio = document.querySelector('#year').value;
+
+function esBisiesto() {
+	if (anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0) {
+		document.querySelector('#resultado').innerHTML = "El año " + anio + " es bisiesto";
+	} else {
+		document.querySelector('#resultado').innerHTML = "El año " + anio + " NO es bisiesto";
+	}
+}
