1fbaa52bb76d6279788cde81f72c95f408e0d0f3
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-11-25 13:02:33 -0600

    remove problem description

--- 02-algorithms/04-primer-duplicado/main.js
+++ 02-algorithms/04-primer-duplicado/main.js
@@ -1,23 +1,4 @@
-/* El 1er elemento duplicado.-
-
-Dado un array que contiene sólo números en un rango de 1 a una longitud, encontrar el 1er número duplicado para el que la 2da aparición tiene el índice mínimo.
-
-En otras palabras, si hay más de un número duplicado, devuelve el número para el que la 2da aparición tiene un índice más pequeño que la 2da aparición del otro número.
-
-Si no hay tales elementos, devolver -1
-
-- Ejemplo, para:
-a = [2, 1, 3, 5, 3, 2];
-
-- La salida debe ser:
-firstDuplicate(a) = 3;
-
-- Hay 2 duplicados: 2 y 3.
-La 2da aparición de 3 tiene un índice más pequeño que la 2da aparición de 2, por lo que la respuesta es 3.
-
-Para a = [2, 4, 3, 5, 1], la salida debe ser firstDuplicate(a) = -1;
-Para a = [2, 1, 3, 1, 3, 2], la salida debe ser firstDuplicate(a) = 1;
-*/
+// El 1er elemento duplicado.-
 
 function firstDuplicate(a) {
     // Objeto vacío para ir agregando en key:value, el número visitado en la iteración. (numero:true)
