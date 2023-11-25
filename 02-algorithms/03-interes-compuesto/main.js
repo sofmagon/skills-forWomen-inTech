82425346d4ef6e11bfa0f776803f65bc77e09ac1
Sofía Martinelli <ada_hack545@womenintech.mx>
2023-11-25 12:57:31 -0600

    remove problem description

--- 02-algorithms/03-interes-compuesto/main.js
+++ 02-algorithms/03-interes-compuesto/main.js
@@ -1,25 +1,4 @@
-/* Interés compuesto de una cuenta bancaria.-
-
-Se ha depositado una cantidad específica de dinero (deposit), cada año el saldo incrementa a la misma tasa de crecimiento (rate).
-Calcular cuánto tiempo le tomaría al saldo superar un umbral específico (threshold).
-
-- Ejemplo, para:
-deposit = 100;
-rate = 20;
-threshold = 170;
-
-- La salida debe ser:
-depositProfit(deposit, rate, threshold) = 3;
-
-Cada año la cantidad de dinero en la cuenta crece 20%, quedando en 3 años de la siguiente forma:
-
-year 0: 100
-year 1: 120
-year 2: 144
-year 3: 172.8
-
-Es decir, tomaría 3 años alcanzar el objetivo.
-*/
+// Interés compuesto de una cuenta bancaria
 
 function depositProfit(deposit, rate, threshold) {
     let yearCount = 0;
@@ -42,4 +21,4 @@ function depositProfit(deposit, rate, threshold) {
     return yearCount;
 }
 
-console.log(depositProfit(10000, 6, 15000));
+console.log(depositProfit(100, 20, 170));
