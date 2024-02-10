136e125e67dbe7d98a9e7c0d55f2231aa1c4e9ed
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-02-10 12:51:20 -0600

    add: JSX structure

---
+++ 03-pilotaje/src/layout/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+	return (
+		<footer class="footer">
+			<div class="footer__contenedor">
+				<p class="copyright">2024 &copy; Todos Los Derechos Reservados | Skills For Women In Tech</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
