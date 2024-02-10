ede3d492cff48f0c554d3dc1105ce4fc3956c944
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-02-10 13:06:48 -0600

    add: improving routing & imports

--- 03-pilotaje/src/App.jsx
+++ 03-pilotaje/src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './layout/Navigation/Navbar'
+import Home from './layout/Home/Home'
+import Footer from './layout/Footer/Footer'
 
 function App() {
 
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
-				<Route />
+				<Route exact path='/' element={<Home />} />
 				<Route />
 				<Route />
 			</Routes>
+			<Footer />
 		</BrowserRouter>
 	)
 }
