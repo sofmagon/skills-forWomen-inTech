9f3e111bd24fdb99a0d87295376d31cbe8f1dda9
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-02-12 11:09:48 -0600

    feat: .map method & object destructuring

--- 03-pilotaje/src/components/Listado/ListadoLugares.jsx
+++ 03-pilotaje/src/components/Listado/ListadoLugares.jsx
@@ -15,28 +15,33 @@ const ListadoLugares = () => {
 		<main className="todos todos__contenedor">
 			<h1 className="todos__heading">Listado completo</h1>
 			<section className="todos__grid">
-				<div className="lugar">
-					<img src="https://picsum.photos/1000/700" alt="Imagen favorito 01" />
-					<div className="lugar__textos">
-						<h3>Nombre Lugar 01</h3>
-						<p>
-							DESCRIPCIÓN. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Architecto aliquam possimus ut commodi sit error.
-						</p>
-						<p>
-							DOMICILIO con todo lo que implica desde la base de datos: Calle y
-							Número. Colonia. Códgio Postal. Ciudad. Estado.
-						</p>
-						<div className="botones">
-							<a className="boton boton--editar" href="./editar.html">
-								Editar
-							</a>
-							<a className="boton boton--borrar" href="#">
-								Borrar
-							</a>
+				{
+					lugares.map(({ id, nombre, descripcion, caller, numero, colonia, cp, estado, foto }) => {
+
+						<div key={id} className="lugar">
+							<img src="https://picsum.photos/1000/700" alt="Imagen favorito 01" />
+							<div className="lugar__textos">
+								<h3>Nombre Lugar 01</h3>
+								<p>
+									DESCRIPCIÓN. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Architecto aliquam possimus ut commodi sit error.
+								</p>
+								<p>
+									DOMICILIO con todo lo que implica desde la base de datos: Calle y
+									Número. Colonia. Códgio Postal. Ciudad. Estado.
+								</p>
+								<div className="botones">
+									<a className="boton boton--editar" href="./editar.html">
+										Editar
+									</a>
+									<a className="boton boton--borrar" href="#">
+										Borrar
+									</a>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
+					})
+				}
 			</section>
 		</main>
 	)
