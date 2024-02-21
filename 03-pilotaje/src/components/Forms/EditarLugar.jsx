a46dbd6f0fa8658ae3e40cff16474c835fd5b022
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-02-21 12:35:48 -0600

    add: name attribute on every input

--- 03-pilotaje/src/components/Forms/EditarLugar.jsx
+++ 03-pilotaje/src/components/Forms/EditarLugar.jsx
@@ -45,35 +45,35 @@ const EditarLugar = () => {
 					<legend>Edita los datos</legend>
 					<div className="campo">
 						<label htmlFor="nombre">Nombre:</label>
-						<input type="text" id="nombre" required="" />
+						<input type="text" id="nombre" name='nombre' required="" />
 					</div>
 					<div className="campo">
 						<label htmlFor="descripcion">Descripción:</label>
-						<textarea id="descripcion" defaultValue={""} />
+						<textarea id="descripcion" name='descripcion' defaultValue={""} />
 					</div>
 					<div className="campo">
 						<label htmlFor="callenum">Calle y Número:</label>
-						<input type="text" id="callenum" required="" />
+						<input type="text" id="callenum" name='callenum' required="" />
 					</div>
 					<div className="campo">
 						<label htmlFor="colonia">Colonia:</label>
-						<input type="text" id="colonia" required="" />
+						<input type="text" id="colonia" name='colonia' required="" />
 					</div>
 					<div className="campo">
 						<label htmlFor="cp">Código Postal:</label>
-						<input type="text" id="cp" required="" />
+						<input type="text" id="cp" name='cp' required="" />
 					</div>
 					<div className="campo">
 						<label htmlFor="ciudad">Ciudad:</label>
-						<input type="text" id="ciudad" required="" />
+						<input type="text" id="ciudad" name='ciudad' required="" />
 					</div>
 					<div className="campo">
 						<label htmlFor="estado">Estado:</label>
-						<input type="text" id="estado" required="" />
+						<input type="text" id="estado" name='estado' required="" />
 					</div>
 					<div className="campo">
 						<label htmlFor="estado">Fotografía:</label>
-						<input type="file" id="fotografia" accept="image/png, image/jpeg" />
+						<input type="file" id="fotografia" name='fotografia' accept="image/png, image/jpeg" />
 					</div>
 				</fieldset>
 				<button className='boton boton--form'>Realizar cambios</button>
