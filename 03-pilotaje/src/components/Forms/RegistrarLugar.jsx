2dcb1515c52cf7773fbf5021f8596cf4405ddf14
Sofía Martinelli <ada_hack545@womenintech.mx>
2024-02-26 13:11:46 -0600

    feat: onInputChange function

--- 03-pilotaje/src/components/Forms/RegistrarLugar.jsx
+++ 03-pilotaje/src/components/Forms/RegistrarLugar.jsx
@@ -20,6 +20,14 @@ const RegistrarLugar = () => {
 
 	const { nombre, descripcion, calle, numero, colonia, cp, ciudad, estado, foto } = lugar;
 
+	// para cada input en su atributo onChange
+	const onInputChange = (e) => {
+		// función set: 1.- spread operator para expandir los atributos del objeto lugar. 2.- para el evento que se generó: recuperar el elemento que lo disparó y su valor.
+
+		// si coincide con alguna de las propiedades del objeto lugar, se modificará su valor y se actualizará el objeto automáticamente
+		setLugar({ ...lugar, [e.target.name]: e.target.value })
+	}
+
 	return (
 		<main className="registrar registrar__contenedor">
 			<h1 className="registrar__heading">Registra tu lugar seguro</h1>
