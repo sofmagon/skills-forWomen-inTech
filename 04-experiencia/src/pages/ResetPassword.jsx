e52a34a9275d908950e09fafc3f85e32bce39eac
SOFMAGON <ada_hack545@womenintech.mx>
2024-06-26 12:30:20 -0600

    change: branding

---
+++ 04-experiencia/src/pages/ResetPassword.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import AuthImage from '../images/auth-image.jpg';
+import salmonLogo from '../images/logos/NVG-salmon.png';
+import rocketIcon from '../images/icons/NVG-rocket-icon.png';
+
+function ResetPassword() {
+  return (
+    <main className="bg-white dark:bg-slate-900">
+
+      <div className="relative md:flex">
+
+        {/* Content */}
+        <div className="md:w-1/2">
+          <div className="min-h-[100dvh] h-full flex flex-col after:flex-1">
+
+            {/* Header */}
+            <div className="flex-1">
+              <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+                {/* Logo */}
+                <Link className="block" to="/">
+                  <img src={salmonLogo} width={100} className='mt-8' alt="New Ventures Logo" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="max-w-sm mx-auto w-full px-4 py-8">
+              <h1 className="text-3xl text-slate-800 dark:text-slate-100 font-bold mb-6">Reset your Password</h1>
+              {/* Form */}
+              <form>
+                <div className="space-y-4">
+                  <div>
+                    <label className="block text-sm font-medium mb-1" htmlFor="email">Email Address <span className="text-rose-500">*</span></label>
+                    <input id="email" className="form-input w-full" type="email" />
+                  </div>
+                </div>
+                <div className="flex justify-end mt-6">
+                  <button className="btn bg-[#00BCB4] hover:bg-[#000] text-white whitespace-nowrap">Send Reset Link</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        {/* Image */}
+        <div className="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
+          <img className="object-cover object-center w-full h-full" src={AuthImage} width="760" height="1024" alt="Authentication" />
+          <img className="absolute top-1/4 left-0 -translate-x-1/2 ml-0 hidden lg:block" src={rocketIcon} width="125" height="210" alt="Authentication decoration" />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default ResetPassword;
